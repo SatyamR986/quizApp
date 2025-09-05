179047b7b6736669c4fab9fc2df2ab442652c089
SatyamR <sregmi986@gmail.com>
2025-09-05 11:12:22 +0545

    git UI components created

---
+++ features/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => (
+    <header className='text-center mb-4'>
+        <h1 className='text 3xl font-bold text-gray-800'>Quiz App</h1>
+    </header>
+);
+export default Header;
---
+++ features/components/MainLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MainLayout = ({ children }) => {
+    return(
+        <div className="j">
+            {children}
+        </div>
+    );
+};
+export default MainLayout;
